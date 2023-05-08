f570bf56354bc89731ec49b034306213cf564e1c
parlinduha <dhparlin@gmail.com>
2023-05-08 23:01:02 +0700

    add logo.tsx

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -7,7 +7,7 @@ import {useRouter} from 'next/navigation';
 const Logo = () => {
     const router = useRouter();
     return ( 
-        <Image alt="Logo" className="hidden md:block cursor-pointer" height="100" width="100" src="/images/logo.png"/>
+        <Image alt="Logo" className="hidden md:block cursor-pointer" height="100" width="100" src="/images/logo.png" />
      );
 }
 
