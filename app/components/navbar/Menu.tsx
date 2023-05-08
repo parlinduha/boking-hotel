c2107148803aeb58c125a5fdb79727804edfcccc
parlinduha <dhparlin@gmail.com>
2023-05-08 23:02:58 +0700

    modify Menu

--- app/components/navbar/Menu.tsx
+++ app/components/navbar/Menu.tsx
@@ -1,4 +1,6 @@
 "use client";
+import {AiOutlineMenu} from "react-icons/ai"
+import Avatar from "../Avatar";
 
 const Menu = () => {
   return (
@@ -7,10 +9,15 @@ const Menu = () => {
         <div
           onClick={() => {}}
           className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer"
-        >git 
+        >
           Menu
         </div>
-        <div className=""></div>
+        <div onClick={()=>{}} className="p-4 md:py-2 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition">
+            <AiOutlineMenu/>
+            <div className="hidden md:block">
+                <Avatar/>
+            </div>
+        </div>
       </div>
     </div>
   );
