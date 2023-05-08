0bd9785fc9b014d748c85106216862dc922cc822
parlinduha <dhparlin@gmail.com>
2023-05-08 23:01:58 +0700

    add Avatar component

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -6,7 +6,7 @@ import Search from "./Search";
 const Navbar = () => {
   return (
     <div className="fixed w-full bg-white z-10 shadow-sm">
-        <div className="py-0 border-b-[1px]">
+        <div className="py-4 border-b-[1px]">
             {/* Container */}
             <Container>
                 <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
