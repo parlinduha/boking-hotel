dbb58e1e3b0c1366932272ad6577466bf8611efa
parlinduha <dhparlin@gmail.com>
2023-05-12 00:23:55 +0700

    menambah menu item list

--- app/components/navbar/Menu.tsx
+++ app/components/navbar/Menu.tsx
@@ -3,9 +3,18 @@ import {AiOutlineMenu} from "react-icons/ai"
 import Avatar from "../Avatar";
 import {useCallback, useState} from "react"
 import MenuItem from "./MenuItem";
+import { SafeUser } from "app/types";
+import { useRouter } from "next/navigation";
 
-const Menu = () => {
+
+interface UserMenuProps {
+  currentUser?: SafeUser | null
+}
+
+const Menu: React.FC<UserMenuProps>  = () => {
     const [isOpen, setIsOpen] = useState(false);
+
+    const router = useRouter();
 
     const toggleOpen = useCallback(() => {
       setIsOpen((value) => !value);
@@ -15,7 +24,6 @@ const Menu = () => {
     <div className="relative">
       <div className="flex flex-row items-center gap-3">
         <div
-          onClick={}
           className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer"
         >
           Menu
@@ -30,7 +38,8 @@ const Menu = () => {
       {isOpen && (
         <div className=" absolute rounded-xl  shadow-md w-[40vm] md:w-3/4 bg-white overflow_hidden right-0 top-12 text-sm">
           <>
-            <MenuItem/>
+            <MenuItem  label="Login" onClick={() => router.push('/')}/>
+            <MenuItem  label="Daftar" onClick={() => router.push('/')}/>
           </>
         </div>
       )}
