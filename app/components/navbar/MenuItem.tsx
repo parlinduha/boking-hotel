8e15d4639f433bda67db703e11a4a8a6d88690e9
parlinduha <dhparlin@gmail.com>
2023-05-12 00:24:34 +0700

    menambah props

--- app/components/navbar/MenuItem.tsx
+++ app/components/navbar/MenuItem.tsx
@@ -1,8 +1,26 @@
-const MenuItem = () => {
+"use client"
+interface MenuItemProps {
+    onClick: () => void;
+    label : string
+}
+
+const MenuItem: React.FC<MenuItemProps> = ({
+    onClick,
+    label
+  }) => {
     return ( 
-        <div>
-            Menu item
-        </div>
+        <div 
+      onClick={onClick} 
+      className="
+        px-4 
+        py-3 
+        hover:bg-neutral-100 
+        transition
+        font-semibold
+      "
+    >
+      {label}
+    </div>
      );
 }
  
