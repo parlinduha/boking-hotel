ed72824c29e157d8483cb323850567b441a361fe
parlinduha <dhparlin@gmail.com>
2023-05-15 21:17:38 +0700

    membuat handle submit

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -36,6 +36,14 @@ const Modal: React.FC<ModalProps> = ({
         }, 300)
     }, [disabled, onClose])
 
+    const handleSubmit = useCallback(() => {
+        if (disabled){
+            return;
+        }
+
+        onSubmit();
+    }, [disabled, onSubmit])
+
     return (  );
 }
  
