e9748b0d55a9265b92f2688d36e3664a5a241092
parlinduha <dhparlin@gmail.com>
2023-05-15 21:02:55 +0700

    make interfaces modal props

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -1,5 +1,20 @@
 "use client"
 
+import React from "react";
+
+interface ModalProps {
+    isOpen?: boolean;
+    onClose: () => void;
+    onSubmit: () => void;
+    title?: string;
+    body?: React.ReactElement;
+    footer?: React.ReactElement;
+    actionLabel: string;
+    disabled?: boolean;
+    secondarySecond?: () => void;
+    secondaryLabel?: string; 
+}
+
 const Modal = () => {
     return (  );
 }
